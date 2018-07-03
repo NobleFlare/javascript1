9afeaf80d43f08522699b6b71c41a63fb6d99375
Ethan Barron <ethanwbarron@gmail.com>
2018-07-02 20:36:56 -0700

    day 1 notes

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+//declare a variable
+var firstName;
+
+//define variable
+firstName = "Ethan"; //string
+
+//define and declare your variable
+var lastName = "Barron";
+
+console.log(firstName, lastName);
+
+
+firstName = 'Superman';
+console.log(firstName);
+/*
+this is a block comment or "multiline comment"
+*/
+
+var getDataAsync; //add a comment for varibales to keep track for yourself and other programmers
+
+
+{
+  //New Scope.
+}
