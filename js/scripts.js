981645b4bff044a7a1f74aa7ac1db3880e323ccf
nobleflare <ethanwbarron@gmail.com>
2018-07-05 12:41:23 -0700

    takehome hobby

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,2 @@
-//declare a variable
-var firstName;
-
-//define variable
-firstName = "Ethan"; //string
-
-//define and declare your variable
-var lastName = "Barron";
-
-console.log(firstName, lastName);
-
-
-firstName = 'Superman';
-console.log(firstName);
-/*
-this is a block comment or "multiline comment"
-*/
-
-var getDataAsync; //add a comment for varibales to keep track for yourself and other programmers
-
-
-{
-  //New Scope.
-}
+var aboutMe = "I want to learn javascript and I like to play D&D and D&D related things in my spare time";
+console.log(aboutMe);
