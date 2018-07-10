feff494647fdd55ad806b4bc88fe7fbe12707ba0
Ethan Barron <ethanwbarron@gmail.com>
2018-07-09 21:00:17 -0700

    else and other stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,96 @@
-let firstName = prompt("Please enter your first name.");
-let lastName = prompt("Please enter your last name");
-console.log(firstName);
+let bestNumber = false;
+let isTrue = false;
 
-alert ("Your last name is "+lastName);
-// var today = new Date();
-// var day = today.getDate();
-// var month = today.getMonth()+1;
-// var year = today.getFullYear();
-// var today = month + '/' + day + '/' + year;
-let birthDay = new Date(prompt("When is your birthday? ", "5/21/2000"));
-alert ("The birthday you entered is "+birthDay);
+console.log(bestNumber || isTrue);
 
-let confirmBday = confirm (`Is ${brithday} your correct birthday?`);
-if(confirmBday) alert (`${birthday} is in fact your birthday!`);
+
+console.log(!isTrue);
 
 
 
+
+let num = 11;
+let string = '11';
+console.log(num == string);// loose equality checks for value and not type
+console.log(num === string);// strict equality and checks for both value and type
+
+
+
+
+const prim = 'my string'; //primative
+const objectString = new String('my string');
+console.log(prim === objectString);// false
+console.log(prim == objectString);// true
+
+//Modulus
+console.log(5 % 2); //reurns 1 Modulus(%) is a division remainder.
+
+
+//INC and DEX
+let start = 1;
+//... after some logic
+start++;// taking the value of start and adding 1 to it. start-- removes 1
+// console.log(start.toUpperCase());//toUpperCase makes the value uppercase. This error throws an actual arror
+
+let nonNumber = "cheese";
+console.log(parseInt(nonNumber));
+console.log(isNaN(nonNumber));//isNaN checks if it is not a number
+
+let fifty ="50";
+console.log(parseInt(fifty));
+
+
+let nonString = 50000;
+console.log(nonString.toString());//parseInt and toString take one tupe and convert it into another
+
+console.log(nonString +=20);// nonString = nonString +20
+
+// var first = prompt('Please enter a value', 10);
+// var second = prompt('Please enter ANOTHER value', 10);
+// var first = parseInt(first);
+// var second = parseInt(second);
 //
-// var myString = "Hello World!*78";
-// console.log(typeof myString);
-//
-// var myNumber = "43"; //primitive
-// var myOtherNumber = new Number(43);
-//
-// console.log(typeof myNumber, myOtherNumber);
-//
-// var isLoggedIn = false; //boolean
-// let newVariable = "I am the best Variable";
-// console.log(newVariable, 'after');
-//
-// {
-// let newVariable = " I am the worst...";
-// console.log(newVariable, "inside the scope");
+// if(!isNaN(first) && !isNaN(second)) {
+// alert(first+second);
+// } else if(isNaN(second)){
+//   second = parseInt(prompt('No, a Numericle number', 10));
+//   alert(first + second);
+// } else {
+//   alert ('Please write your numbers in numericle format.');
 // }
-// // let newVariable; //declaration
-// newVariable = "my dog is ugly";
-// console.log(newVariable, "outsite the scope");
-// isLoggedIn = null;
-//
-// //string interpolation
-// console.log(" Hey there, " + myString +"how are you?"); //es5
-// console.log(`hey there, ${myString} how are you?`);//es6
-//
-// {
-//   //block of code
+
+//switch
+
+// let greeting = prompt('tell me how to say hello', 'Gutentaag!');
+// switch(greeting.toLowerCase()){ //normalizing data!
+//     case 'hello':
+//       alert(`I love saying ${greeting}!`);
+//       break;
+//     case 'wassup':
+//       alert(`${greeting} is SO 90s!`);
+//       break;
+//     default:
+//       alert(`I have never heard of ${greeting} before!`);
+//       break;
 // }
-//
-//
-//
-//
-//
-//
-//
-//
-// const age = 32;
-//
-// {
-//   const age =35;
-//   console.log(age);
-// }
-// console.log(age);
-//
-// let today = new Date();
-// let myBirthday = new Date("1/1/19");
-// console.log(myBirthday);
-//
-//
-// let bestNumber = 42.7;
-//
-// Math.floor(bestNumber);
-// console.log(Math.floor(bestNumber) * Math.PI);
-//
-//
-//
-//
-//
-// let classRoom = "Room 1";
-// let objectClassRoom = new String ("Room 1");
-// console.log(typeof classRoom);
-// console.log(typeof objectClassRoom);
-// console.log(classRoom.length);
-//
-//
-// //stuff for take home
-// alert("hey there user!, Pick a path...");//1-direction communicatino to your user
-// const isReady = confirm("Are you ready to party?");
-// alert(isReady);
-//
-// let userName = prompt("What is your name?", "John Doe");//if left empty the second part acts like a place holder
-// console.log(userName);
-//
-// //make gh pages  settings tab of your repository. Drop down thing there and se it as master.
+
+//Ternary
+// let money =10;
+// let currency = confirm('In dollars or not');
+// currency ? alert (`$${money}`) : alert(money);
+
+
+
+let isAdult = confirm ('Are you 18 or over in age?');
+if(isAdult){
+  let isUnder80 = confirm('are you under 80 years old?');
+  if (isUnder80){
+    let isCool = confirm('Do you like star wars?');
+    if(!isCool) return;
+    alert("you are so cool!");
+  } else {
+    let isElderly = confirm("Do you enjoy prunes?";)
+  }
+} else {
+  alert('You were shot first!!!')
+}
