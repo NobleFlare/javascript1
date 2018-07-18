16aec5b0292c8e5018d2261fa707c23c156021d7
nobleflare <ethanwbarron@gmail.com>
2018-07-18 15:36:01 -0700

    not real takehome, just the try it out

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
+let humanYears = prompt("How old is your dog?")
+
+function dogAge(years){
+  return years *7;
+};
+
+if (isNaN(humanYears) || humanYears == 0) {
+  alert("Please enter a non-zero number.");
+}else{
+  alert(dogAge(humanYears));
+};
+
+///Other part of the try it out
+let personAge = prompt("Please enter your age.")
+let prodAmount = prompt("How much of this product do you use per day?")
+
+function lifeTime(supply, age){
+  let restOfLife = 80-age;
+  let totalSupply = restOfLife * 365 * supply;
+  return alert(`You will need ${totalSupply} to last you until you're 80`);
+}
+if (isNaN(personAge || prodAmount)){
+  alert("Please only enter a number for both entries.")
+}else{
+  lifeTime(prodAmount, personAge);
+};
+
 // //ANONYMOUS FUNCION
 // //FUNCTION DEFINITION
 // // function (string){//most basit function// inside the () is where your arguments/paramaters go
@@ -32,29 +59,29 @@
 // //confirmStuff("do you want to watch Frozen?");
 
 
-//RECURSION
-let i = 0;
-  function iCallMyself(){
-    console.log(i);
-    i++;
-    iCallMyself();
-  }
-//FAT ARROW Functions
-//BASIC IMPPLEMENTATION
-  (logFunction, alertFunction) => {//no function keyword
-    logFunction('hello world');
-    alertFunction('goodbye, friends!');
-  }
-
-  //ONLY 1 arguments
-  string => {//parens are option
-    console.log(string);
-  }
-
-  let logString = string =>{
-    console.log(logString);
-  };
-
-  //logString("hello cats!");
-  //IMPLICIT RETURN
-  string => (string.toUpperCase());
+// //RECURSION
+// let i = 0;
+//   function iCallMyself(){
+//     console.log(i);
+//     i++;
+//     iCallMyself();
+//   }
+// //FAT ARROW Functions
+// //BASIC IMPPLEMENTATION
+//   (logFunction, alertFunction) => {//no function keyword
+//     logFunction('hello world');
+//     alertFunction('goodbye, friends!');
+//   }
+//
+//   //ONLY 1 arguments
+//   string => {//parens are option
+//     console.log(string);
+//   }
+//
+//   let logString = string =>{
+//     console.log(logString);
+//   };
+//
+//   //logString("hello cats!");
+//   //IMPLICIT RETURN
+//   string => (string.toUpperCase());
