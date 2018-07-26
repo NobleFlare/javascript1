675a0096440b9362c8945a0320d9ddedb5de2e27
Ethan Barron <ethanwbarron@gmail.com>
2018-07-25 18:45:56 -0700

    take home after review

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,53 @@
-//More dom stuff!!!
-const div = document.querySelector('.myDiv');
-//console.log(div.textContent); //returns text (only) in All Children
-//div.textContent = 'eat cheese and die!!!';
-console.log(div.innerHTML);
-div.innerHTML = "<h1>Hey there, R.L Stein. Did you die in the 90s?";
+const body = document.querySelector("body");
+const script = document.querySelector("script");
+let header = document.createElement("header");
+let main = document.createElement("main");
+let footer = document.createElement("footer");
+//NAV
+let nav = document.createElement("ul");
 
-let newH1 = document.createElement("h1");
-newH1.id += " red_bg";
-div.appendChild(newH1);// always adds after the lsast element in the parent
+/*
+Creates an Li for the Dom.
+expects list of classes (strink)
+returns newElement appended to Nav
+*/
+function createLi(classArray, linkText){
+  let newLink = document.createElement("li");
+  let aTag = document.createElement("a");
+  aTag.setAttribute("href", "#");
+  aTag.textContent = linkText;
+  newLink.appendChild(aTag);
+  for(let i = 0; i < classArray.length; i++){
+    newLink.classList.add(classArray[i]);
+  }
+  newLink.classList.add(classArray);
+  nav.appendChild(newLink);
+}
+createLi(["pull-left", "active"], "Home");
+createLi(["pull-right"], "Contact");
+createLi(["pull-right"], "About");
+nav.classList.add("nav","nav-pills");
 
-let newH2 = document.createElement("h2");
-newH2.textContent = "I am before the h1!!";
-newH2.className = "blue_bg";
-//newH2.insertBefore("white-text");
-div.insertBefore(newH2, newH1);
+header.appendChild(nav);
+body.insertBefore(header, script);
 
-let newP = document.createElement("p");
-newP.textContent = "I like Pie";
-newP.classList.add('purple-text', 'green-bg');
-div.appendChild(newP);
 
-//Takehome
-//do the try it out "clone repository"
+
+
+
+let bodyDiv = document.createElement("div");
+bodyDiv.classList.add("col-sm-6", "col-sm-offset-3", "text-center");
+
+function createTextElement(elementType, text) {
+  let newElement = document.createElement(elementType);
+  newElement.textContent = text;
+  return newElement;
+
+};
+let h1 = createTextElement("h1", "Hello World");
+let pTag = createTextElement("p", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+pTag.className = "text-justify";
+bodyDiv.appendChild(h1);
+bodyDiv.appendChild(pTag);
+body.insertBefore(bodyDiv, script);
